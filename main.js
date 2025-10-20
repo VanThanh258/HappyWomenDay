f9f1d03954013adc07924de5262e8b62223305ba
VanThanh258 <bachvanthanh98@gmail.com>
2025-10-20 19:03:50 +0700

    update

--- main.js
+++ main.js
@@ -4,14 +4,3 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
-
-const params = new URLSearchParams(window.location.search);
-
-// Lấy giá trị của param "img"
-const newSrc = params.get("img");
-
-// Nếu có param img -> thay src của thẻ img
-if (newSrc) {
-  const img = document.getElementById("dynamic-image");
-  img.src = newSrc;
-}
