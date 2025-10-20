7be5d8ad53cdc5b278748bf99564a8bf72bde521
VanThanh258 <bachvanthanh98@gmail.com>
2025-10-20 18:08:55 +0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+const params = new URLSearchParams(window.location.search);
+
+// Lấy giá trị của param "img"
+const newSrc = params.get("img");
+
+// Nếu có param img -> thay src của thẻ img
+if (newSrc) {
+  const img = document.getElementById("dynamic-image");
+  img.src = newSrc;
+}
